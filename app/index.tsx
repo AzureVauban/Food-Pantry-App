550eab340c9ebd8c3debedd7f4eb9ae81cfb1d22
Ethan <lyg23@txstate.edu>
2025-09-22 14:55:19 -0500

    chore: Added home page for login and edited layouts in _layout.tsx and index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
-import { Text, View } from 'react-native';
+import { Link } from "expo-router";
+import { View, Text, Button } from "react-native";
 
-export default function Index() {
+
+export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to Food Inventory App</Text>
+      <Link href="/login" asChild>
+        <Button title="Go to Login" />
+      </Link>
     </View>
   );
 }
